f1669a3ad057598acfd52fe4b4d8dbdc44fb8c38
ublubu <kynan.rilee@gmail.com>
2017-08-15 15:15:48 -0400

    querying an endpoint!

--- haskube-client/app/Main.hs
+++ haskube-client/app/Main.hs
@@ -5,16 +5,26 @@
 module Main where
 
 import           Control.Applicative
-import           Control.Lens              ((^?), _Just)
+import           Control.Lens              ((.~), (?~), (^?), _Just)
 import           Control.Monad
+import           Control.Monad.Except
 import           Control.Monad.Trans.Maybe
 
+import qualified Data.Aeson                as AE
 import           Data.Aeson.Lens           (key, nth, _String)
-import qualified Data.Aeson.Lens           as Y
+import qualified Data.Aeson.Lens           as AE
+import           Data.ByteString           (ByteString)
+import qualified Data.ByteString           as B
+import qualified Data.ByteString.Lazy      as BL
 import           Data.Monoid
 import qualified Data.Text                 as T
+import           Data.Text.Encoding        (encodeUtf8)
 import qualified Data.Yaml                 as Y
 
+import           Network.Connection        (TLSSettings (..))
+import           Network.HTTP.Client       (newManager)
+import           Network.HTTP.Client.TLS
+import           Network.Wreq
 import           System.Environment        (getEnv)
 import           Turtle
 
@@ -27,6 +37,7 @@ main = do
                  Just (server, token) -> do
                    print server
                    print token
+                   print =<< runExceptT (doot server token)
 
 getToken :: MonadIO m => Text -> MaybeT m Text
 getToken secretId = MaybeT $ do
@@ -54,3 +65,17 @@ script = do
   token <- getToken secretId
   server <- getServer
   return (server, token)
+
+mkOptions :: Text -> Options
+mkOptions token =
+  defaults & auth ?~ oauth2Bearer (encodeUtf8 token)
+
+doot :: Text -> Text -> ExceptT JSONError IO (Response AE.Value)
+doot server token = do
+  let manSets = mkManagerSettings (TLSSettingsSimple True False False) Nothing
+  man <- liftIO $ newManager manSets
+  let opts = defaults
+        & auth ?~ oauth2Bearer (encodeUtf8 token)
+        & manager .~ Right man
+  r <- liftIO $ getWith opts $ T.unpack server <> "/api"
+  asJSON r
